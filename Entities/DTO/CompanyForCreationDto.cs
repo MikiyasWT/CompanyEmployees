5315b685e92e126960d74126ece33c03d0449614
MikiyasWT <mikiywendu44@gmail.com>
2024-05-01 15:43:04 +0300

    company creation with employees done.

--- Entities/DTO/CompanyForCreationDto.cs
+++ Entities/DTO/CompanyForCreationDto.cs
@@ -7,4 +7,8 @@ public class CompanyForCreationDto
  public string Name { get; set; }
  public string Address { get; set; }
  public string Country { get; set; }
+
+ public IEnumerable<EmployeeForCreationDto> Employees {get; set;}
 }
+
+
